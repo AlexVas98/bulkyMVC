6d654e018c233763a65cd267a64a7e45ddeaa9d4
AlexVas98 <alexandros.basilakis98@gmail.com>
2023-11-23 23:45:14 +0200

    35

---
+++ bulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using bulkyWeb.Data;
+using bulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;//Μια μεταβλητη που περναμε μέσα local var για να το προβαλουμε
+        public CategoryController(ApplicationDbContext db)//οτι παρουμε απο τον controller το κανουμε assign σε local var
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
